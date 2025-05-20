26d9659c3e71ae4c753941414053c837ff1db014
☕ Rômulo F. Douro ☕ <rfdouro@gmail.com>
2025-05-20 10:09:53 -0300

    Update Aplicacao.java

--- src/main/java/br/org/rfdouro/exemplospringrestapi02/Aplicacao.java
+++ src/main/java/br/org/rfdouro/exemplospringrestapi02/Aplicacao.java
@@ -6,6 +6,8 @@ package br.org.rfdouro.exemplospringrestapi02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
